2d76e8265232a452ef796e80f1a24dadc2bd5fc1
Christopher Davies <christopher.davies@genomicsengland.co.uk>
2025-07-18 13:02:43 +0100

    adding docker support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import { publishPythonPackage } from './languages/python.js';
+import { publishDockerImage } from './languages/docker.js';
 
 async function run() {
   try {
@@ -7,10 +8,15 @@ async function run() {
     const apiToken = core.getInput('api_token', { required: true });
     const hashId = core.getInput('hash_id', { required: true });
     const packageDir = core.getInput('package_dir', { required: true });
+    const dockerTag = core.getInput('docker_tag', { required: false });
+    const registryName = core.getInput('registry_name', { required: false });
 
     switch (packageType.toLowerCase()) {
       case 'python':
         await publishPythonPackage({ apiToken, hashId, packageDir });
+        break;
+      case 'docker':
+        await publishDockerImage({ apiToken, hashId, registryName, dockerTag });
         break;
       default:
         core.setFailed(`Unsupported package_type: ${packageType}`);
